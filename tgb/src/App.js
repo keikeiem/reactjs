74e8dad1df9ca60f4b7b93105312182c9e06ee5e
keikeiem <kimkm@stclab.com>
2021-03-10 19:10:25 +0900

    Basic Route Config for SPA

--- tgb/src/App.js
+++ tgb/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GG WP Learn React Just Like
-        </a>
-      </header>
-    </div>
+    // <>
+    //   <Route path="/" component={Home} exact={true} />
+    //   <Route path="/about" component={About} />
+    // </>
+    <>
+      <ul>
+        <li>
+          {
+          /*
+            SPA에서는 Link를 사용해서 경로이동함
+            새로고침이 없는 모양
+          */
+          }
+          <Link to="/">Go Home</Link>
+        </li>
+        <li>
+          <Link to="/about">Go About</Link>
+        </li>
+        <li>
+          <Link to="/info">Go Info</Link>
+        </li>
+      </ul>
+      <Route path="/" component={Home} exact={true} />
+      {/* 하나의 Component에 여러개의 path를 지정할 때 */}
+      <Route path={['/about', '/info']} component={About} />
+    </>
   );
-}
+};
 
 export default App;
---
+++ tgb/src/components/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div>
+        Route: Home
+    </div>
+  );
+}
+
+export default Home;
